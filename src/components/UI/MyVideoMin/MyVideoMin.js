e9893836e9f68e5b6f0dcabbb3c00a5ea690ea6e
DmitryFaber <mistertwister698@gmail.com>
2022-11-16 19:49:41 +0700

    add props for MyVideoMin

--- src/components/UI/MyVideoMin/MyVideoMin.js
+++ src/components/UI/MyVideoMin/MyVideoMin.js
@@ -5,9 +5,10 @@ import styles from "./MyVideoMin.module.css";
 const MyVideoMin = (props) => {
   return (
     <div className={styles.video_min}>
-      <VideoPreview src={props.src} width="200px" />
+      <VideoPreview src={props.src} width="200" />
       <h3 className={styles.video_name}>{props.video_name}</h3>
-      <p className={styles.views}>{props.views} просмотров</p>
+      {/* <p className={styles.views}>{props.views} просмотров</p> */}
+      <p className={styles.views}>0 просмотров</p>
       <p className={styles.video_time}>{props.video_time}</p>
     </div>
   );
