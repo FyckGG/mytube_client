d3e1a82fabf589c93e7f7b644d0e0fb6f0340ed5
DmitryFaber <mistertwister698@gmail.com>
2022-11-21 16:48:19 +0700

    add render video stats when responce

--- src/pages/WatchVideoPage/WatchVideo.js
+++ src/pages/WatchVideoPage/WatchVideo.js
@@ -7,8 +7,9 @@ import ProfilePicture from "../../components/UI/ProfilePicture/ProfilePicture";
 import Main_Button from "../../components/UI/main_button/Main_Button";
 import LikeDislikePanel from "../../components/LikeDislikePanel/LikeDislikePanel";
 import axios from "axios";
+import { observer } from "mobx-react-lite";
 
-const WatchVideo = () => {
+const WatchVideo = observer(() => {
   const store = React.useContext(Context);
   const [videoPath, setVideoPath] = React.useState("");
   const [videoName, setVideoName] = React.useState("");
@@ -21,16 +22,17 @@ const WatchVideo = () => {
   const [countSubs, setCountSubs] = React.useState("");
   const [countViews, setCountViews] = React.useState("");
   const [searchParams, setSearchParams] = useSearchParams();
+  const [pageLoading, setPageLoading] = React.useState(false);
 
   React.useEffect(() => {
     const getVideo = async () => {
+      setPageLoading(true);
       const video = await axios.post(
         "http://localhost:5000/user-action/load-watch-video",
         {
           video_id: searchParams.get("v"),
         }
       );
-      console.log(video);
       setVideoPath(video.data.video.video_directory);
       setVideoName(video.data.video.video_name);
       setVideoDescription(video.data.video.video_description);
@@ -41,6 +43,7 @@ const WatchVideo = () => {
       setCountDislike(video.data.count_dislikes);
       setCountSubs(video.data.count_subs);
       setCountViews(video.data.count_views);
+      setPageLoading(false);
     };
     getVideo();
   }, []);
@@ -56,38 +59,49 @@ const WatchVideo = () => {
           style={{ display: "inline-block", marginTop: "20px" }}
         />
       </div>
-      <div style={{ borderBottom: "4px solid #d9d9d9", paddingBottom: "10px" }}>
-        <div className={styles.video_stat}>
-          <h1 className={styles.video_name}>{videoName}</h1>
-          <h2 className={styles.views_count}>
-            Количество просмотров:{countViews}
-          </h2>
-        </div>
-        <LikeDislikePanel likes={countLike} dislikes={countDislike} />
+      {pageLoading ? (
+        <div></div>
+      ) : (
+        <>
+          <div
+            style={{ borderBottom: "4px solid #d9d9d9", paddingBottom: "10px" }}
+          >
+            <div className={styles.video_stat}>
+              <h1 className={styles.video_name}>{videoName}</h1>
+              <h2 className={styles.views_count}>
+                Количество просмотров:{countViews}
+              </h2>
+            </div>
+            <LikeDislikePanel likes={countLike} dislikes={countDislike} />
 
-        <div className={styles.channel_picture}>
-          <ProfilePicture
-            src={`http://localhost:5000${channelAvatar}`}
-            alt="profile_img"
-            width="60"
-            height="60"
-          />
-        </div>
-        <div className={styles.channel_info}>
-          <h3 className={styles.channel_name}>{channelName}</h3>
-          <p className={styles.count_subs}>{countSubs} subs.</p>
-        </div>
-        <div className={styles.subscribe_button}>
-          <Main_Button>Подписаться</Main_Button>
-        </div>
-      </div>
-      <div className={styles.video_description}>
-        <h3>Описание видео:</h3>
-        <p>{videoDescription}</p>
-        <h3 style={{ marginTop: "5px" }}>Категория: {videoSubject}</h3>
-      </div>
+            <div className={styles.channel_picture}>
+              <ProfilePicture
+                src={`http://localhost:5000${channelAvatar}`}
+                alt="profile_img"
+                width="60"
+                height="60"
+              />
+            </div>
+            <div className={styles.channel_info}>
+              <h3 className={styles.channel_name}>{channelName}</h3>
+              <p className={styles.count_subs}>
+                {pageLoading ? "" : `${countSubs} subs.`}
+              </p>
+            </div>
+
+            <div className={styles.subscribe_button}>
+              <Main_Button>Подписаться</Main_Button>
+            </div>
+          </div>
+          <div className={styles.video_description}>
+            <h3>Описание видео:</h3>
+            <p>{videoDescription}</p>
+            <h3 style={{ marginTop: "5px" }}>Категория: {videoSubject}</h3>
+          </div>
+        </>
+      )}
     </div>
   );
-};
+});
 
 export default WatchVideo;
