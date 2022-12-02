3d400d5a38e064b9cad8252f0a032f32c76657a5
DmitryFaber <mistertwister698@gmail.com>
2022-12-02 18:30:15 +0700

    add postcommentform component

---
+++ src/components/PostCommentForm/PostCommentForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./PostCommentForm.module.css";
+
+const PostCommentForm = (props) => {
+  const handlePostFormChange = () => {
+    props.on_cancel();
+  };
+
+  const handleCommentTextChange = (e) => {
+    props.on_comment(e.target.value);
+  };
+
+  const sendComment = () => {
+    props.on_send();
+  };
+  return (
+    <div className={styles.post_form}>
+      <div className={styles.post_pole}>
+        <textarea
+          rows="3"
+          cols="60"
+          name="text"
+          className={styles.text_area}
+          onChange={handleCommentTextChange}
+        ></textarea>
+      </div>
+      <div className={styles.form_buttons}>
+        <button className={styles.cancel} onClick={handlePostFormChange}>
+          Отмена
+        </button>
+        <button className={styles.send} onClick={sendComment}>
+          Отправить
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PostCommentForm;
