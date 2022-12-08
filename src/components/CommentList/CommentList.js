1a2aae38d1ffe11873e1670e6be32bcf316d9086
DmitryFaber <mistertwister698@gmail.com>
2022-12-08 20:50:56 +0700

    add comments map in comment list

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Comment from "../Comment/Comment";
 import styles from "./CommentList.module.css";
+import timeAgo from "./../../otherServices/timeAgo";
 
-const CommentList = () => {
+const CommentList = (props) => {
   return (
     <div>
-      <Comment />
-      <Comment />
-      <Comment />
+      {props.comment_list.map((comment) => (
+        <div className={styles.comment}>
+          <Comment
+            user_name={comment.user_name}
+            comment_text={comment.comment_text}
+            user_profile={`http://localhost:5000${comment.user_avatar}`}
+            comment_time={timeAgo(comment.comment_date)}
+          />
+        </div>
+      ))}
     </div>
   );
 };
