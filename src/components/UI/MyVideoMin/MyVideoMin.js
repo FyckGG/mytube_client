8af8af7f8fbf3ec6531c6adc28bae1a7df2fb497
DmitryFaber <mistertwister698@gmail.com>
2022-12-09 19:27:43 +0700

    add in video mins true count of views

--- src/components/UI/MyVideoMin/MyVideoMin.js
+++ src/components/UI/MyVideoMin/MyVideoMin.js
@@ -1,14 +1,15 @@
 import React from "react";
 import VideoPreview from "../VideoPreview/VideoPreview";
 import styles from "./MyVideoMin.module.css";
+import convertCount from "./../../../otherServices/ConvertCount";
 
 const MyVideoMin = (props) => {
   return (
     <div className={styles.video_min}>
       <VideoPreview src={props.src} width="200" />
       <h3 className={styles.video_name}>{props.video_name}</h3>
-      {/* <p className={styles.views}>{props.views} просмотров</p> */}
-      <p className={styles.views}>0 просмотров</p>
+
+      <p className={styles.views}>{convertCount(props.video_views)} просм.</p>
       <p className={styles.video_time}>{props.video_time}</p>
     </div>
   );
