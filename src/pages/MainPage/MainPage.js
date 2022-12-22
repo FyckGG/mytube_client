2437b4367a52c1bb849b2b31a26f013405662593
DmitryFaber <mistertwister698@gmail.com>
2022-12-22 20:40:03 +0700

    update MainPage for videos render

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./MainPage.module.css";
+import VideoMin from "../../components/UI/VideoMin/VideoMin";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
+  const arr = [1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1];
   return (
     <div className={styles.main_page}>
-      <h1>MainPage</h1>
+      {arr.map(() => (
+        <div className={styles.video_min}>
+          <Link
+            to={
+              "/watch_video?v=638494a1fe301e648c2bb148&u=63779401330ba70b9cc7dd97"
+            }
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <VideoMin />
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
