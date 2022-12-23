daeafe0339c6bf13dd842b816d6f8485eea67839
DmitryFaber <mistertwister698@gmail.com>
2022-12-23 19:15:41 +0700

    add tooltip and some updates for videomin components

--- src/components/UI/MyVideoMin/MyVideoMin.js
+++ src/components/UI/MyVideoMin/MyVideoMin.js
@@ -3,12 +3,15 @@ import VideoPreview from "../VideoPreview/VideoPreview";
 import styles from "./MyVideoMin.module.css";
 import convertCount from "./../../../otherServices/ConvertCount";
 import timeAgo from "./../../../otherServices/timeAgo";
+import Tooltip from "../Tooltip/Tooltip";
 
 const MyVideoMin = (props) => {
   return (
     <div className={styles.video_min}>
       <VideoPreview src={props.src} width="200" />
-      <h3 className={styles.video_name}>{props.video_name}</h3>
+      <Tooltip text={props.video_name}>
+        <h3 className={styles.video_name}>{props.video_name}</h3>
+      </Tooltip>
       <p className={styles.views}>{convertCount(props.video_views)} просм.</p>
       <p className={styles.video_time}>{props.video_time}</p>
       <p>{timeAgo(props.video_date)}</p>
--- src/components/UI/VideoMin/VideoMin.jsx
+++ src/components/UI/VideoMin/VideoMin.jsx
@@ -4,6 +4,7 @@ import styles from "./VideoMin.module.css";
 import convertCount from "./../../../otherServices/ConvertCount";
 import timeAgo from "./../../../otherServices/timeAgo";
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
+import Tooltip from "../Tooltip/Tooltip";
 import { Link } from "react-router-dom";
 
 const VideoMin = (props) => {
@@ -16,7 +17,10 @@ const VideoMin = (props) => {
         />
       </div>
       <div className={styles.video_info}>
-        <Link to={"/profile/63779401330ba70b9cc7dd97"}>
+        <Link
+          to={"/profile/63779401330ba70b9cc7dd97"}
+          style={{ height: "40%" }}
+        >
           <ProfilePicture
             src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
             alt="profile_img"
@@ -25,15 +29,17 @@ const VideoMin = (props) => {
           />
         </Link>
         <div className={styles.video_stats}>
-          <h2 className={styles.video_name}>videoName..</h2>
+          <Tooltip text={"video_name"}>
+            <h2 className={styles.video_name}>videoName..</h2>
+          </Tooltip>
           <Link
             to={"/profile/63779401330ba70b9cc7dd97"}
             style={{ color: "inherit", textDecoration: "inherit" }}
           >
             <h4 className={styles.channel_name}>channelName</h4>
           </Link>
-          <p className={styles.views}>{convertCount(228)} просм.</p>
-          <p className={styles.video_time}>{"3m 5s"}</p>
+          <p className={styles.views}>{convertCount(228000)} просм.</p>
+          <p className={styles.video_time}>{"3h 3m 5s"}</p>
           <p>{timeAgo("2022-12-14T13:58:40.809+00:00")}</p>
         </div>
       </div>
