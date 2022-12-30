199b75a8d8e772aa66de8e729aaae271aa609689
DmitryFaber <mistertwister698@gmail.com>
2022-12-30 12:06:32 +0700

    update mainpage for render videos from server

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,23 +2,52 @@ import React from "react";
 import styles from "./MainPage.module.css";
 import VideoMin from "../../components/UI/VideoMin/VideoMin";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const MainPage = () => {
-  const arr = [1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1];
+  const [videos, setVideos] = React.useState([]);
+  const [videosLoading, setVideosLoading] = React.useState(false);
+  React.useEffect(() => {
+    const getVideos = async () => {
+      setVideosLoading(true);
+      const videos = await axios.get(
+        "http://localhost:5000/data-load/get-videos"
+      );
+      console.log(videos);
+      setVideos(videos.data);
+      setVideosLoading(false);
+    };
+    getVideos();
+  }, []);
+
   return (
     <div className={styles.main_page}>
-      {arr.map(() => (
-        <div className={styles.video_min}>
-          <Link
-            to={
-              "/watch_video?v=638494a1fe301e648c2bb148&u=63779401330ba70b9cc7dd97"
-            }
-            style={{ color: "inherit", textDecoration: "inherit" }}
-          >
-            <VideoMin />
-          </Link>
-        </div>
-      ))}
+      {videosLoading ? (
+        <div>Загрузка...</div>
+      ) : (
+        <>
+          {videos.map((video) => (
+            <div className={styles.video_min}>
+              <Link
+                //to={"/watch_video?v=638494a1fe301e648c2bb148"}
+                to={`watch_video?v=${video.video_id}`}
+                style={{ color: "inherit", textDecoration: "inherit" }}
+              >
+                <VideoMin
+                  name={video.video_name}
+                  preview={video.video_preview}
+                  channel={video.channel_id}
+                  channel_name={video.channel_name}
+                  channel_icon={video.channel_avatar}
+                  views={video.count_views}
+                  date={video.video_date}
+                  duration={video.video_duration}
+                />
+              </Link>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
