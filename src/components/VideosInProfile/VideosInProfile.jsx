43449faac43788decbfe2e0e9b908abe6ebf0407
DmitryFaber <mistertwister698@gmail.com>
2022-12-31 17:32:07 +0700

    add new component for render videos in user profile

---
+++ src/components/VideosInProfile/VideosInProfile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MyVideoMin from "../UI/MyVideoMin/MyVideoMin";
+import styles from "./VideosInProfile.module.css";
+import convertTime from "../../otherServices/convertVideoTime";
+
+export const VideosInProfile = (props) => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      {props.videos
+        .slice(props.currentVideoPage * 24, (props.currentVideoPage + 1) * 24)
+        .map((video) => (
+          <div className={styles.video_min}>
+            <Link
+              to={`/watch_video?v=${video.id}`}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <MyVideoMin
+                src={`http://localhost:5000${video.thumbnail_dir}`}
+                video_name={video.video_name}
+                video_time={convertTime(video.video_duration)}
+                video_views={video.number_views}
+                video_date={video.video_date}
+              />
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
