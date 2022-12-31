1fe4943425ec3f06902e4b755688ef06e59349f9
DmitryFaber <mistertwister698@gmail.com>
2022-12-31 17:31:09 +0700

    add clicked mode for main button

--- src/components/UI/main_button/Main_Button.js
+++ src/components/UI/main_button/Main_Button.js
@@ -3,7 +3,10 @@ import "./Main_Button.css";
 
 export default function Main_Button(props) {
   return (
-    <button className="main_button" onClick={props.button_action}>
+    <button
+      className={props.is_pressed ? "pressed_main_button" : "main_button"}
+      onClick={props.button_action}
+    >
       {props.children}
     </button>
   );
