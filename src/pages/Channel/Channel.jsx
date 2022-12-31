bbc28adb4a741c1a4ec5148219ce0c3a032169ee
DmitryFaber <mistertwister698@gmail.com>
2022-12-31 17:42:05 +0700

    add two components for render profiles

---
+++ src/pages/Channel/Channel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserProfile from "../UserProfile/UserProfile";
+
+export const Channel = () => {
+  return <UserProfile is_my_profile={false} />;
+};
+
+export default Channel;
---
+++ src/pages/MyChannel/MyChannel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserProfile from "../UserProfile/UserProfile";
+
+const MyChannel = () => {
+  return <UserProfile is_my_profile={true} />;
+};
+
+export default MyChannel;
