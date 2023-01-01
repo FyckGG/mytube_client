17341619098b9e8853686f8376fe02bfb18c7d11
DmitryFaber <mistertwister698@gmail.com>
2023-01-01 13:43:54 +0700

    update channel information for render user data

--- src/components/ChannelInformation/ChannelInformation.js
+++ src/components/ChannelInformation/ChannelInformation.js
@@ -1,17 +1,26 @@
 import React from "react";
+import styles from "./ChannelInformation.module.css";
+import convertDate from "../../otherServices/convertDate";
+import addSpaces from "../../otherServices/addSpaces";
 
-const ChannelInformation = () => {
+const ChannelInformation = (props) => {
   return (
-    <div>
+    <div className={styles.channel_info}>
       <h2>Описание канала</h2>
-      <p>
-        Минет по 40 рублей Минет по 40 рублей Минет по 40 рублей Минет по 40
-        рублей Минет по 40 рублей Минет по 40 рублей Минет по 40 рублей Минет по
-        40 рублей Минет по 40 рублей Минет по 40 рублей Минет по 40 рублей Минет
-        по 40 рублей Минет по 40 рублей Минет по 40 рублей Минет по 40 рублей
-        Минет по 40 рублей Минет по 40 рублей Минет по 40 рублей Минет по 40
-        рублей Минет по 40 рублей Минет по 40 рублей (шутка)
-      </p>
+      <div className={styles.info_text}>
+        <p className={styles.description}>Здесь могло быть ваше описание</p>
+        <div className={styles.channel_stats}>
+          <div className={styles.channel_stats_item}>
+            Дата регистрации: {convertDate(props.reg_date)}
+          </div>
+          <div
+            className={styles.channel_stats_item}
+            style={{ marginTop: "5px" }}
+          >
+            Кол-во просмотров: {addSpaces(props.count_views)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
