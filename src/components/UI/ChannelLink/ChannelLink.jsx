ec1013e34c1c92e02c4d7920115a8d64f930a86f
DmitryFaber <mistertwister698@gmail.com>
2023-01-01 18:53:35 +0700

    create new component link on channel

---
+++ src/components/UI/ChannelLink/ChannelLink.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ChannelLink = ({ children, user_id, channel_id, style }) => {
+  return (
+    <Link
+      to={
+        user_id === channel_id
+          ? `/profile/${channel_id}`
+          : `/channel/${channel_id}`
+      }
+      style={style}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default ChannelLink;
