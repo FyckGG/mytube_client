fafb565fc4dff02f210387063b28743803f8df00
DmitryFaber <mistertwister698@gmail.com>
2023-01-04 19:09:13 +0700

    update userlikes components

--- src/pages/UserLikes/UserLikes.js
+++ src/pages/UserLikes/UserLikes.js
@@ -1,9 +1,25 @@
 import React from "react";
+import UserDataLoad from "../../userDataLoad/userDataLoad";
+import { VIdeoMinList } from "../../components/UI/VideoMinList/VIdeoMinList";
+import { getLastUrlPart } from "../../otherServices/getLastUrlPart";
+import { Donut_2 } from "../../components/UI/Donut_2/Donut_2";
+import styles from "./UserLikes.module.css";
 
 const UserLikes = () => {
+  const [likedVideos, setLikedVideos] = React.useState([]);
+  const [isVIdeosLoad, setIsVideosLoad] = React.useState(true);
+  React.useEffect(() => {
+    const getLikedVideos = async () => {
+      const user_id = getLastUrlPart(window.location.href);
+      const liked_videos = await UserDataLoad.getLikedVIdeos(user_id);
+      setLikedVideos(liked_videos.data);
+      setIsVideosLoad(false);
+    };
+    getLikedVideos();
+  }, []);
   return (
-    <div>
-      <h1>UserLikes</h1>
+    <div className={styles.user_likes_page}>
+      {isVIdeosLoad ? <Donut_2 /> : <VIdeoMinList videos={likedVideos} />}
     </div>
   );
 };
