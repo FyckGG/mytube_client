41f910b35fac4d85f823bc359ae2fcea745613cf
DmitryFaber <mistertwister698@gmail.com>
2023-01-06 15:38:18 +0700

    update for vertical dots

--- src/components/UI/VerticalDots/VerticalDots.jsx
+++ src/components/UI/VerticalDots/VerticalDots.jsx
@@ -3,13 +3,31 @@ import styles from "./VerticalDots.module.css";
 
 export const VerticalDots = (props) => {
   const [contentActive, setContentActive] = React.useState(false);
+  const dotsRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!contentActive) return;
+    const handleClick = (e) => {
+      if (!dotsRef.current) {
+        return;
+      }
+      if (!dotsRef.current.contains(e.target)) {
+        setContentActive(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [contentActive, setContentActive]);
+
   const showContent = (e) => {
     e.preventDefault();
     setContentActive(!contentActive);
   };
   return (
     <div>
-      <div className={styles.dropdown} onClick={showContent}>
+      <div className={styles.dropdown} onClick={showContent} ref={dotsRef}>
         <ul
           className={`${styles.dropbtn} ${styles.icons} ${styles.btn_right} ${styles.showleft}`}
         >
@@ -24,9 +42,18 @@ export const VerticalDots = (props) => {
               ? `${styles.dropdown_content} ${styles.dropdown_content_active}`
               : styles.dropdown_content
           }
+          ref={dotsRef}
         >
           {props.content.map((item) => (
-            <div className={styles.content_item}>{item.name}</div>
+            <div
+              className={styles.content_item}
+              onClick={() => {
+                item.action();
+                setContentActive(false);
+              }}
+            >
+              {item.name}
+            </div>
           ))}
         </div>
       </div>
