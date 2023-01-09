885f3cb30ce42f776a25291cb7179d24c4178668
DmitryFaber <mistertwister698@gmail.com>
2023-01-09 17:51:22 +0700

    add donut in channelinfo

--- src/components/ChannelInformation/ChannelInformation.js
+++ src/components/ChannelInformation/ChannelInformation.js
@@ -2,25 +2,33 @@ import React from "react";
 import styles from "./ChannelInformation.module.css";
 import convertDate from "../../otherServices/convertDate";
 import addSpaces from "../../otherServices/addSpaces";
+import { Donut_2 } from "../UI/Donut_2/Donut_2";
 
 const ChannelInformation = (props) => {
+  console.log("views " + props.count_views);
   return (
     <div className={styles.channel_info}>
-      <h2>Описание канала</h2>
-      <div className={styles.info_text}>
-        <p className={styles.description}>Здесь могло быть ваше описание</p>
-        <div className={styles.channel_stats}>
-          <div className={styles.channel_stats_item}>
-            Дата регистрации: {convertDate(props.reg_date)}
+      {props.reg_date == "" || props.count_views == "" ? (
+        <Donut_2 />
+      ) : (
+        <>
+          <h2>Описание канала</h2>
+          <div className={styles.info_text}>
+            <p className={styles.description}>Здесь могло быть ваше описание</p>
+            <div className={styles.channel_stats}>
+              <div className={styles.channel_stats_item}>
+                Дата регистрации: {convertDate(props.reg_date)}
+              </div>
+              <div
+                className={styles.channel_stats_item}
+                style={{ marginTop: "5px" }}
+              >
+                Кол-во просмотров: {addSpaces(props.count_views)}
+              </div>
+            </div>
           </div>
-          <div
-            className={styles.channel_stats_item}
-            style={{ marginTop: "5px" }}
-          >
-            Кол-во просмотров: {addSpaces(props.count_views)}
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
