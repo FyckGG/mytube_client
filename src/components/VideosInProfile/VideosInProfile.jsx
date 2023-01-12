2cdbcb7632c33c3b3b6cdf7b21db4652e3c4bdbb
DmitryFaber <mistertwister698@gmail.com>
2023-01-12 15:28:44 +0700

    update videosinprofile for render videos without pages

--- src/components/VideosInProfile/VideosInProfile.jsx
+++ src/components/VideosInProfile/VideosInProfile.jsx
@@ -5,28 +5,30 @@ import styles from "./VideosInProfile.module.css";
 import convertTime from "../../otherServices/convertVideoTime";
 
 export const VideosInProfile = (props) => {
+  const s1 = props.currentVideoPage ? props.currentVideoPage * 24 : 0;
+  const s2 = props.currentVideoPage
+    ? (props.currentVideoPage + 1) * 24
+    : props.videos.length;
   return (
     <div style={{ textAlign: "center" }}>
-      {props.videos
-        .slice(props.currentVideoPage * 24, (props.currentVideoPage + 1) * 24)
-        .map((video) => (
-          <div className={styles.video_min}>
-            <Link
-              to={`/watch_video?v=${video.id}`}
-              style={{ color: "inherit", textDecoration: "inherit" }}
-            >
-              <MyVideoMin
-                video_id={video.id}
-                src={`http://localhost:5000${video.thumbnail_dir}`}
-                video_name={video.video_name}
-                video_time={convertTime(video.video_duration)}
-                video_views={video.number_views}
-                video_date={video.video_date}
-                is_watch_later={video.is_watch_later}
-              />
-            </Link>
-          </div>
-        ))}
+      {props.videos.slice(s1, s2).map((video) => (
+        <div className={styles.video_min}>
+          <Link
+            to={`/watch_video?v=${video.id}`}
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <MyVideoMin
+              video_id={video.id}
+              src={`http://localhost:5000${video.thumbnail_dir}`}
+              video_name={video.video_name}
+              video_time={convertTime(video.video_duration)}
+              video_views={video.number_views}
+              video_date={video.video_date}
+              is_watch_later={video.is_watch_later}
+            />
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
