71be5c8522934d9961aad5eba5878f90c94c6e6f
DmitryFaber <mistertwister698@gmail.com>
2023-01-17 13:58:22 +0700

    add edit video form and edit video page

---
+++ src/components/EditVideoForm/EditVideoForm.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import styles from "./EditVideoForm.module.css";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../..";
+import userActions from "../../userActions/userActions";
+
+export const EditVideoForm = observer((props) => {
+  const navigate = useNavigate();
+  const [videoDescription, setVideoDescription] = React.useState(
+    props.default_description
+  );
+  const store = React.useContext(Context);
+  const [videoName, setVideoName] = React.useState(props.default_name);
+  const [videoAccess, setVideoAccess] = React.useState(props.default_access);
+  const [error, setError] = React.useState("");
+  // const [videoSubject, setVideoSubject] = React.useState(props.default_subject);
+
+  const changeVideo = async (e) => {
+    e.preventDefault();
+    if (videoName == "" || videoDescription == "") {
+      setError("При изменении видео не должно быть пустых строк.");
+      return;
+    }
+    setError("");
+    const edit_result = await userActions.editVideo(
+      props.video_id,
+      videoName,
+      videoDescription,
+      videoAccess
+    );
+    navigate(`/profile/${store.user.id}`);
+  };
+
+  React.useEffect(() => {
+    setVideoName(props.default_name);
+    setVideoDescription(props.default_description);
+    setVideoAccess(props.default_access);
+    // setVideoSubject(props.default_subject);
+  }, [props.is_loading]);
+
+  return (
+    <div className={styles.edit_video_form}>
+      <div>
+        <h1>Изменить данные видео:</h1>
+        <form onSubmit={changeVideo}>
+          <label for="video_name">
+            Название видео:
+            <input
+              type={"text"}
+              value={videoName}
+              onChange={(e) => {
+                setVideoName(e.target.value);
+              }}
+            />
+          </label>
+          <br />
+          <label for="video_description">
+            Описание видео:
+            <textarea
+              rows="3"
+              cols="45"
+              name="text"
+              className={styles.video_description}
+              value={videoDescription}
+              onChange={(e) => {
+                setVideoDescription(e.target.value);
+              }}
+            />
+          </label>
+          <br />
+          <label>
+            Тип доступа:
+            <select
+              defaultValue={videoAccess}
+              value={videoAccess}
+              onChange={(e) => {
+                setVideoAccess(e.target.value);
+              }}
+            >
+              <option value={true}>Публичный</option>
+              <option value={false}>Приватный</option>
+            </select>
+          </label>
+          <br />
+          {/* <label>
+              Тема видео:
+              <select
+                defaultValue={videoSubject}
+                value={videoSubject}
+                onChange={(e) => {
+                  setVideoSubject(e.target.value);
+                }}
+              >
+                <option value={"Фильмы/сериалы"}>Фильмы/сериалы</option>
+                <option value={"Мультфилмы/анимация"}>
+                  Мультфилмы/анимация
+                </option>
+                <option value={"Музыка"}>Музыка</option>
+                <option value={"Видеоигры"}>Видеоигры</option>
+                <option value={"Спорт"}>Спорт</option>
+                <option value={"Активный отдых"}>Активный отдых</option>
+                <option value={"Наука/технологиии"}>Наука/технологии</option>
+                <option value={"Экономика"}>Экономика</option>
+                <option value={"Политика"}>Политика</option>
+              </select>
+            </label> */}
+          <div className={styles.error_message}>{error}</div>
+          <input
+            className={`${styles.submit_button} ${styles.finish_button}`}
+            type={"submit"}
+            id={"save_changes"}
+            value={"Сохранить изменения"}
+          />
+          <input
+            className={`${styles.cancel_button} ${styles.finish_button}`}
+            type={"button"}
+            id={"cancel_changes"}
+            value={"Отменить"}
+            onClick={() => {
+              navigate(`/profile/${store.user.id}`);
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
+});
---
+++ src/pages/EditVideoPage/EditVideoPage.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { Donut_2 } from "../../components/UI/Donut_2/Donut_2";
+import { EditVideoForm } from "../../components/EditVideoForm/EditVideoForm";
+import VideoDataLoad from "../../videoDataLoad/videoDataLoad";
+import { observer } from "mobx-react-lite";
+import { Context } from "../..";
+
+export const EditVideoPage = observer(() => {
+  const store = React.useContext(Context);
+  const [isUserLoading, setIsUserLoading] = React.useState(store.isLoading);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [videoName, setVideoName] = React.useState();
+  const [videoDescription, setVideoDescription] = React.useState();
+  const [videoAccess, setVideoAccess] = React.useState();
+  const [videoSubject, setVideoSubject] = React.useState();
+  React.useEffect(() => {
+    const getVideoData = async () => {
+      if (isUserLoading == false) {
+        const video_data = await VideoDataLoad.getVideoForEdit(
+          searchParams.get("v"),
+          store.user.id
+        );
+        setVideoName(video_data.data.name);
+        setVideoDescription(video_data.data.description);
+        setVideoAccess(video_data.data.access_type);
+        setVideoSubject(video_data.data.subject);
+        setIsLoading(false);
+        return;
+      }
+
+      setIsUserLoading(store.isLoading);
+      setTimeout(() => {
+        setIsUserLoading(store.isLoading);
+      }, 5000);
+
+      // setVideoName(video_data.data.name);
+      // setVideoDescription(video_data.data.description);
+      // setVideoAccess(video_data.data.access_type);
+      // setVideoSubject(video_data.data.subject);
+      //setIsLoading(false);
+    };
+    getVideoData();
+  }, [isUserLoading]);
+  return (
+    <div style={{ textAlign: "center", padding: "5px" }}>
+      {isLoading ? (
+        <Donut_2 />
+      ) : (
+        <EditVideoForm
+          video_id={searchParams.get("v")}
+          is_loading={isLoading}
+          default_name={videoName}
+          default_description={videoDescription}
+          default_access={videoAccess}
+          default_subject={videoSubject}
+        />
+      )}
+    </div>
+  );
+});
