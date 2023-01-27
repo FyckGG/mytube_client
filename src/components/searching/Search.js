6c9b27fd65695831145b8fb16bc4bd21b96719b2
DmitryFaber <mistertwister698@gmail.com>
2023-01-27 15:39:15 +0700

    update search

--- src/components/searching/Search.js
+++ src/components/searching/Search.js
@@ -2,13 +2,46 @@ import React from "react";
 import styles from "./Search.module.css";
 import { useMediaQuery } from "react-responsive";
 import Main_Button from "../UI/main_button/Main_Button";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 export default function Search() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [stringSearch, setStringSearch] = React.useState(
+    searchParams.get("params")
+  );
+  const [stringParams, setStringParams] = React.useState();
+  const navigate = useNavigate();
+  const search = () => {
+    //navigate(`/search-results?params=${stringParams}`);
+    ///console.log(window.location.href);
+    if (window.location.href.includes("/search-results"))
+      window.location.reload();
+  };
   return (
     <form className={styles.search}>
-      <input type="text" placeholder="Search"></input>
-
-      <Main_Button>Find</Main_Button>
+      <input
+        type="text"
+        maxLength={50}
+        value={stringSearch}
+        onChange={(e) => {
+          setStringSearch(e.target.value);
+          const str_par_1 = e.target.value.replace(/ /g, "+");
+          const str_par_2 = str_par_1.replace(/#/g, "%23");
+          setStringParams(str_par_2);
+        }}
+        placeholder="Search"
+      ></input>
+      <Link to={`/search-results?params=${stringParams}`}>
+        <Main_Button
+        // button_action={() => {
+        //   search();
+        // }}
+        >
+          Find
+        </Main_Button>
+      </Link>
     </form>
   );
 }
