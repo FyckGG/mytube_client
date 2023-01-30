4fed6ee8901c9e9d5937d2cd37d6a18d5705e3a6
DmitryFaber <mistertwister698@gmail.com>
2023-01-30 17:49:21 +0700

    update burger menu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./BurgerMenu.module.css";
+import { Link } from "react-router-dom";
 
 export default function BurgerMenu(props) {
   return (
@@ -10,13 +11,25 @@ export default function BurgerMenu(props) {
           : styles.menu_content
       }
     >
-      <div className={styles.menu_header}>{props.header}</div>
+      <div className={`${styles.menu_header} ${styles.bottom_border}`}>
+        {props.header}
+      </div>
       <ul>
-        {props.items.map((item) => (
-          <li>
-            <a href={item.href}>{item.value}</a>
-          </li>
-        ))}
+        {props.items.map((item, item_index, all_items) =>
+          item.href === "" && item.value === "line" ? (
+            <></>
+          ) : item_index + 1 < all_items.length &&
+            all_items[item_index + 1].href === "" &&
+            all_items[item_index + 1].value === "line" ? (
+            <Link to={item.href}>
+              <li className={styles.bottom_border}>{item.value}</li>
+            </Link>
+          ) : (
+            <Link to={item.href}>
+              <li>{item.value}</li>
+            </Link>
+          )
+        )}
       </ul>
     </div>
   );
