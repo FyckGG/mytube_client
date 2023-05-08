f4ab26384b0d32ef5b842f6688b5ce564903b920
DmitryFaber <mistertwister698@gmail.com>
2023-05-08 12:34:23 +0700

    add new notFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return <h1 className={styles.not_found_page}>Страница не найдена</h1>;
+};
+
+export default NotFoundPage;
